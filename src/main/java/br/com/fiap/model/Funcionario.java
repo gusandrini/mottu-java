0c3817dca310460d374b5821d224d80988e4f2e7
dudatiemiak <rm554756@fiap.com.br>
2025-05-08 23:35:08 -0300

    Criando classe model Funcionario é obrigatório ligação com alguma filial

---
+++ src/main/java/br/com/fiap/model/Funcionario.java
@@ -0,0 +1,33 @@
+package br.com.fiap.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "funcionario")
+public class Funcionario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_funcionario;
+	@ManyToOne
+	@JoinColumn(name = "id_filial", nullable = false)
+    @NotNull(message = "O funcionário deve estar relacionado a uma filial!")
+	private Filial filial;
+	@NotEmpty(message = "O nome do funcionário deve ser informado")
+    @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres")
+	private String nm_funcionario;
+	@Email(message = "O e-mail deve ser válido")
+    @Size(max = 100, message = "O e-mail deve ter no máximo 100 caracteres")
+	private String nm_email_corporativo;
+	@Size(max = 225, message = "A senha deve ter no máximo 225 caracteres")
+	private String nm_senha;
+}
