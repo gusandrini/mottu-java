b04efed08c6f624d9962cf741b96294e9ae3ae71
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 11:23:22 -0300

    Criada ClienteRepository com métodos que buscam cliente por nome, cpf ou email

---
+++ src/main/java/br/com/fiap/repository/ClienteRepository.java
@@ -0,0 +1,22 @@
+package br.com.fiap.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.fiap.model.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+	@Query("from Cliente c where c.nm_cliente like concat('%', :nome, '%')")
+	List<Cliente> buscarClientePorNome(String nome);
+	
+	@Query(nativeQuery = true, value = """
+			select * from cliente c 
+			where c.nm_cliente like concat('%', :sub_cliente, '%')
+				or c.nr_cpf like concat('%', :sub_cliente, '%')
+				or c.nm_email like concat('%', :sub_cliente, '%')
+			""")
+	public List<Cliente> buscarClientePorSubstring(String sub_cliente);
+}
