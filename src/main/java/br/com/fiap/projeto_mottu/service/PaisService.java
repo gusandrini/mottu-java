ef346f28037f392e10488ffc33e61c036672c967
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 20:41:05 -0300

    Salvando alterações PaisService

--- src/main/java/br/com/fiap/projeto_mottu/service/PaisService.java
+++ src/main/java/br/com/fiap/projeto_mottu/service/PaisService.java
@@ -1,5 +1,26 @@
 package br.com.fiap.projeto_mottu.service;
 
-public class PaisService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.fiap.projeto_mottu.dto.PaisDTO;
+import br.com.fiap.projeto_mottu.model.Pais;
+
+@Service
+public class PaisService {
+	@Autowired
+	private PaisCachingService cacheP;
+	
+	@Transactional(readOnly = true)
+	public Page<PaisDTO> paginar(PageRequest req){
+		
+		Page<Pais> paginas_paises = cacheP.findAll(req);
+		Page<PaisDTO> paginas_paises_dto = 
+				paginas_paises.map(pais -> new PaisDTO(pais));
+		return paginas_paises_dto;
+		
+	}
 }
