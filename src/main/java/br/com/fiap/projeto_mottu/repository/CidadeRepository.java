b513713c146613bfbb29e00f64d1d8fa764f12cb
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 20:31:13 -0300

    Salvando alterações CidadeRepository

--- src/main/java/br/com/fiap/projeto_mottu/repository/CidadeRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/CidadeRepository.java
@@ -1,5 +1,9 @@
 package br.com.fiap.projeto_mottu.repository;
 
-public interface CidadeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.projeto_mottu.model.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
 
 }
