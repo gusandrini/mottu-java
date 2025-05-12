4acdf087fd53b7afbecdc8d1f6a18a6f204b31eb
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 17:03:43 -0300

    Adicionando LogradouroRepository com método provisório

---
+++ src/main/java/br/com/fiap/projeto_mottu/repository/LogradouroRepository.java
@@ -0,0 +1,14 @@
+package br.com.fiap.projeto_mottu.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.fiap.projeto_mottu.model.Logradouro;
+
+public interface LogradouroRepository extends JpaRepository<Logradouro, Long>{
+	
+	@Query("SELECT l FROM Logradouro l WHERE LOWER(l.nm_logradouro) LIKE LOWER(CONCAT('%', :nome, '%'))")
+    List<Logradouro> buscarPorNome(String nome);
+}
