7d7e7ffe121e2775309a050e41d924900e118f29
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 12:03:49 -0300

    Criando ClienteCachingService - revisar depois

---
+++ src/main/java/br/com/fiap/service/ClienteCachingService.java
@@ -0,0 +1,42 @@
+package br.com.fiap.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.model.Cliente;
+import br.com.fiap.repository.ClienteRepository;
+
+@Service
+public class ClienteCachingService {
+
+	@Autowired
+	private ClienteRepository repC;
+	
+	@Cacheable(value = "buscarTodosCache")
+	public List<Cliente> findAll(){
+		return repC.findAll();
+	}
+	
+	@Cacheable(value = "buscarPorID", key = "#id_cliente")
+	public Optional<Cliente> findById(Long id_cliente){
+		return repC.findById(id_cliente);
+	}
+	
+	@Cacheable(value = "buscarPaginasClientes", key = "#req")
+	public Page<Cliente> findAll(PageRequest req){
+		return repC.findAll(req);
+	}
+	
+	@CacheEvict(value = {"buscarTodosCache","buscarPorID", "buscarPaginasClientes"},
+			allEntries = true)
+	public void limparCache() {
+		System.out.println("Limpando cache!");
+	}
+}
