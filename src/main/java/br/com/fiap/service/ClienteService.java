3260b4e2afcb67d48dc3a212eddb1b0aa2290075
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 14:16:08 -0300

    Arrumando paginar de ClienteService

--- src/main/java/br/com/fiap/service/ClienteService.java
+++ src/main/java/br/com/fiap/service/ClienteService.java
@@ -1,5 +1,29 @@
 package br.com.fiap.service;
 
-public class ClienteService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.fiap.dto.ClienteDTO;
+import br.com.fiap.model.Cliente;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteCachingService cacheC;
+	
+	@Transactional(readOnly = true)
+	public Page<ClienteDTO> paginar(PageRequest req){
+		
+		Page<Cliente> paginas_clientes = cacheC.findAll(req);
+		Page<ClienteDTO> paginas_clientes_dto = 
+				paginas_clientes.map(cliente -> new ClienteDTO(cliente));
+		return paginas_clientes_dto;
+		
+	}
+	
+	
 }
