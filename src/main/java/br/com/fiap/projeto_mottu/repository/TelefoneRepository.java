bf4a16531e4d5203f711c109442d15a5ad510952
dudatiemiak <rm554756@fiap.com.br>
2025-05-13 18:38:00 -0300

    Criando TelefoneRepository, por enquanto só com o básico, depois adicionar métodos mais específicos se necessário

---
+++ src/main/java/br/com/fiap/projeto_mottu/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.projeto_mottu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.projeto_mottu.model.Telefone;
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
+
+}
