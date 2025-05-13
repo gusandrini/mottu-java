9c0e1188f1b83f7cadd6d9958d5f224a214b533a
dudatiemiak <rm554756@fiap.com.br>
2025-05-12 21:24:42 -0300

    Arrumando erro, nome errado de tabela na model Pais

--- src/main/java/br/com/fiap/projeto_mottu/model/Pais.java
+++ src/main/java/br/com/fiap/projeto_mottu/model/Pais.java
@@ -12,7 +12,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "cidade")
+@Table(name = "pais")
 public class Pais {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
