c9ca0058402f6c22b2636f4de44d9ea1d16c2077
dudatiemiak <rm554756@fiap.com.br>
2025-05-16 21:48:32 -0300

    Adicionando @Data e tirando construtores, getters e setters

--- src/main/java/br/com/fiap/projeto_mottu/model/Estado.java
+++ src/main/java/br/com/fiap/projeto_mottu/model/Estado.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
 @Table(name = "estado")
+@Data
 public class Estado {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,40 +26,5 @@ public class Estado {
 	@NotEmpty(message = "O nome do estado deve ser informado")
     @Size(min = 2, max = 2, message = "O estado deve conter exatamente 2 letras (UF)")
 	private String nm_estado;
-	
-	public Estado () {}
-
-	public Estado(Long id_estado, @NotNull(message = "O estado deve estar vinculado a um país") Pais pais,
-			@NotEmpty(message = "O nome do estado deve ser informado") @Size(min = 2, max = 2, message = "O estado deve conter exatamente 2 letras (UF)") String nm_estado) {
-		super();
-		this.id_estado = id_estado;
-		this.pais = pais;
-		this.nm_estado = nm_estado;
-	}
-
-	public Long getId_estado() {
-		return id_estado;
-	}
-
-	public void setId_estado(Long id_estado) {
-		this.id_estado = id_estado;
-	}
-
-	public Pais getPais() {
-		return pais;
-	}
-
-	public void setPais(Pais pais) {
-		this.pais = pais;
-	}
-
-	public String getNm_estado() {
-		return nm_estado;
-	}
-
-	public void setNm_estado(String nm_estado) {
-		this.nm_estado = nm_estado;
-	}
-	
 	
 }
