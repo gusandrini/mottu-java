81fb81c2b68d04930313e4ee5e37db4d2a726be8
dudatiemiak <rm554756@fiap.com.br>
2025-05-17 15:40:05 -0300

    Adicionando MotoRepository, só o básico

---
+++ src/main/java/br/com/fiap/projeto_mottu/repository/MotoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.projeto_mottu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.projeto_mottu.model.Moto;
+
+public interface MotoRepository extends JpaRepository<Moto, Long>{
+
+}
