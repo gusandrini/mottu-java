deee793331f8f456c367187a007b8a8f5c350a23
dudatiemiak <rm554756@fiap.com.br>
2025-05-16 21:55:39 -0300

    Adicionando @Data e tirando construtores, getters e setters

--- src/main/java/br/com/fiap/projeto_mottu/model/Pais.java
+++ src/main/java/br/com/fiap/projeto_mottu/model/Pais.java
@@ -4,15 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
 @Table(name = "pais")
+@Data
 public class Pais {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,31 +19,5 @@ public class Pais {
 	@NotEmpty(message = "O nome do país deve ser informado")
     @Size(max = 50, message = "O estado deve ter no máximo 50 caracteres")
 	private String nm_pais;
-	
-	public Pais() {}
-
-	public Pais(Long id_pais,
-			@NotEmpty(message = "O nome do país deve ser informado") @Size(max = 50, message = "O estado deve ter no máximo 50 caracteres") String nm_pais) {
-		super();
-		this.id_pais = id_pais;
-		this.nm_pais = nm_pais;
-	}
-
-	public Long getId_pais() {
-		return id_pais;
-	}
-
-	public void setId_pais(Long id_pais) {
-		this.id_pais = id_pais;
-	}
-
-	public String getNm_pais() {
-		return nm_pais;
-	}
-
-	public void setNm_pais(String nm_pais) {
-		this.nm_pais = nm_pais;
-	}
-	
 	
 }
