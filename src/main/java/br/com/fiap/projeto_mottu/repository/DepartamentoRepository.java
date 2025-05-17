25e822fdf62f150e6ebf1d566d22d50890d32caf
dudatiemiak <rm554756@fiap.com.br>
2025-05-17 15:32:31 -0300

    Adicionando DepartamentoRepository, só o básico

---
+++ src/main/java/br/com/fiap/projeto_mottu/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.projeto_mottu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.projeto_mottu.model.Departamento;
+
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long>{
+
+}
