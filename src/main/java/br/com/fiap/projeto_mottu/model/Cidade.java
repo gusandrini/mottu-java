14b5a2c381add4f7e6da64c45421331e365122d9
dudatiemiak <rm554756@fiap.com.br>
2025-05-16 21:47:11 -0300

    Adicionando @Data e tirando construtores, getters e setters

--- src/main/java/br/com/fiap/projeto_mottu/model/Cidade.java
+++ src/main/java/br/com/fiap/projeto_mottu/model/Cidade.java
@@ -10,9 +10,11 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
 @Entity
 @Table(name = "cidade")
+@Data
 public class Cidade {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,47 +27,4 @@ public class Cidade {
     @Size(max = 50, message = "O nome da cidade deve ter no máximo 50 caracteres")
 	private String nm_cidade;
 
-
-	public Cidade() {}
-
-
-	public Cidade(Long id_cidade, @NotNull(message = "A cidade deve estar vinculada a um estado") Estado estado,
-			@NotEmpty(message = "O nome da cidade deve ser informado") @Size(max = 50, message = "O nome da cidade deve ter no máximo 50 caracteres") String nm_cidade) {
-		super();
-		this.id_cidade = id_cidade;
-		this.estado = estado;
-		this.nm_cidade = nm_cidade;
-	}
-
-
-	public Long getId_cidade() {
-		return id_cidade;
-	}
-
-
-	public void setId_cidade(Long id_cidade) {
-		this.id_cidade = id_cidade;
-	}
-
-
-	public Estado getEstado() {
-		return estado;
-	}
-
-
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
-
-
-	public String getNm_cidade() {
-		return nm_cidade;
-	}
-
-
-	public void setNm_cidade(String nm_cidade) {
-		this.nm_cidade = nm_cidade;
-	}
-	
-	
 }
