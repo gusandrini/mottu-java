2fa38a3cb9c92c7177e3936f667b18ba226858ab
dudatiemiak <rm554756@fiap.com.br>
2025-05-17 15:59:44 -0300

    Adicionando FilialService

---
+++ src/main/java/br/com/fiap/projeto_mottu/service/FilialService.java
@@ -0,0 +1,27 @@
+package br.com.fiap.projeto_mottu.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.fiap.projeto_mottu.dto.FilialDTO;
+import br.com.fiap.projeto_mottu.model.Filial;
+
+@Service
+public class FilialService {
+
+	@Autowired
+	private FilialCachingService cacheC;
+	
+	@Transactional(readOnly = true)
+	public Page<FilialDTO> paginar(PageRequest req){
+		
+		Page<Filial> paginas_filiais = cacheC.findAll(req);
+		Page<FilialDTO> paginas_filiais_dto = 
+				paginas_filiais.map(filial -> new FilialDTO(filial));
+		return paginas_filiais_dto;
+		
+	}
+}
