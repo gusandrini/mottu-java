286aaf4b59bd0a6b262abe38eb912a5a8def9ede
dudatiemiak <rm554756@fiap.com.br>
2025-05-17 15:34:17 -0300

    Adicionando FilialRepository, só o básico

---
+++ src/main/java/br/com/fiap/projeto_mottu/repository/FilialRepository.java
@@ -0,0 +1,9 @@
+package br.com.fiap.projeto_mottu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.projeto_mottu.model.Filial;
+
+public interface FilialRepository extends JpaRepository<Filial, Long>{
+
+}
