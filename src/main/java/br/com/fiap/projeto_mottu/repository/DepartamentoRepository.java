39655cecb417ec89d55165b8c5c0a878464c2f2c
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 18:50:21 -0300

    Atualizando DepartamentoRepository

--- src/main/java/br/com/fiap/projeto_mottu/repository/DepartamentoRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/DepartamentoRepository.java
@@ -1,9 +1,16 @@
 package br.com.fiap.projeto_mottu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.projeto_mottu.model.Departamento;
+import br.com.fiap.projeto_mottu.projection.DepartamentoProjection;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Long>{
 
+	@Query("SELECT d.nmDepartamento AS nmDepartamento, d.descDepartamento AS descDepartamento "
+	         + "FROM Departamento d WHERE d.nmDepartamento = :nmDepartamento ORDER BY d.nmDepartamento DESC")
+	    List<DepartamentoProjection> buscarDepartamentoPorNome(String nmDepartamento);
 }
