76012e5844feec6c7db159a9eb053c085bf1f046
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 18:29:15 -0300

    Atualizando CidadeRepository

--- src/main/java/br/com/fiap/projeto_mottu/repository/CidadeRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/CidadeRepository.java
@@ -1,9 +1,20 @@
 package br.com.fiap.projeto_mottu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.projeto_mottu.model.Cidade;
+import br.com.fiap.projeto_mottu.projection.CidadeProjection;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long>{
 
+	// Busca cidades por ID do estado
+    @Query("select c.nm_cidade as nm_cidade, c.estado.nm_estado as estadoNome from Cidade c where c.estado.id_estado = :idEstado")
+    List<CidadeProjection> buscarPorEstado(Long idEstado);
+
+    // Busca cidades que contenham parte do nome
+    @Query("select c.nm_cidade as nm_cidade, c.estado.nm_estado as estadoNome from Cidade c where lower(c.nm_cidade) like lower(concat('%', :substring, '%'))")
+    List<CidadeProjection> buscarPorNome(String substring);
 }
