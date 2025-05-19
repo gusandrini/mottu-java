e885e37e79dc99ebd0689567891d556f2859eb66
dudatiemiak <rm554756@fiap.com.br>
2025-05-19 20:30:57 -0300

    Adicionando método de busca ordenada em Moto

--- src/main/java/br/com/fiap/projeto_mottu/repository/MotoRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/MotoRepository.java
@@ -11,22 +11,18 @@ import br.com.fiap.projeto_mottu.projection.MotoProjection;
 
 public interface MotoRepository extends JpaRepository<Moto, Long>{
 
-	// Busca motos por situação
-    @Query("SELECT m.nm_placa AS nmPlaca, " +
-           "m.nm_modelo AS nmModelo, " +
-           "m.st_moto AS stMoto, " +
-           "m.cliente.nome AS clienteNome, " +
-           "m.filial_departamento.nome AS filialDepartamentoNome " +
-           "FROM Moto m WHERE m.st_moto = :situacao")
-    List<MotoProjection> buscarPorSituacao(String situacao);
-    
+	// Busca motos por situação, e ordena os resultados pelo campo nm_modelo, por ordem crescente alfabética
+	@Query("SELECT m FROM Moto m WHERE m.st_moto = :situacao ORDER BY m.nm_modelo ASC")
+	List<Moto> buscarPorSituacaoOrdenadoPorModelo(String situacao);
+
     // Busca moto por placa
     @Query("SELECT m FROM Moto m WHERE m.nm_placa = :placa")
     Optional<Moto> buscarPorPlaca(String placa);
 
-    //Busca moto por nome da filial
+    //Busca moto por nome da filial, ordenada por modelo
     @Query("SELECT m.nm_placa AS nmPlaca, m.nm_modelo AS nmModelo, m.st_moto AS stMoto " +
-            "FROM Moto m " +
-            "WHERE m.filial_departamento.filial.nome_filial = :nomeFilial")
-     List<MotoProjection> buscarMotosPorNomeDaFilial(String nomeFilial);
+    	       "FROM Moto m " +
+    	       "WHERE m.filial_departamento.filial.nome_filial = :nomeFilial " +
+    	       "ORDER BY m.nm_modelo ASC")
+    	List<MotoProjection> buscarMotosPorNomeDaFilialOrdenadoPorModelo(String nomeFilial);
 }
