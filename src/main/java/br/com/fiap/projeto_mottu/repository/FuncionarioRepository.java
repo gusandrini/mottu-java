4f85723996744e626bd7e99ce715a1a8bcd8dbde
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 22:28:02 -0300

    Atualizando FuncionarioRepository, adicionando métodos de busca

--- src/main/java/br/com/fiap/projeto_mottu/repository/FuncionarioRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/FuncionarioRepository.java
@@ -1,9 +1,38 @@
 package br.com.fiap.projeto_mottu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.fiap.projeto_mottu.model.Funcionario;
+import br.com.fiap.projeto_mottu.projection.FuncionarioProjection;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 
+	// Busca todos os funcionários de uma filial pelo nome da filial
+	@Query("""
+		    SELECT 
+		        f.nm_funcionario AS nmFuncionario,
+		        f.nm_cargo AS nmCargo,
+		        f.nm_email_corporativo AS nmEmailCorporativo
+		    FROM Funcionario f
+		    WHERE f.filial.nome_filial = :nomeFilial
+		""")
+		List<FuncionarioProjection> buscarFuncionariosPorNomeFilial(@Param("nomeFilial") String nomeFilial);
+
+    // Busca todos os funcionários com determinado cargo
+    @Query("""
+    	    SELECT 
+    	        f.nm_funcionario AS nmFuncionario,
+    	        f.nm_email_corporativo AS nmEmailCorporativo,
+    	        f.filial.nome_filial AS filialNomeFilial
+    	    FROM Funcionario f
+    	    WHERE f.nm_cargo = :cargo
+    	""")
+    	List<FuncionarioProjection> buscarFuncionariosPorCargo(@Param("cargo") String cargo);
+    
+    // Busca funcionário por email corporativo
+    Funcionario findByNmEmailCorporativo(String nmEmailCorporativo);
 }
