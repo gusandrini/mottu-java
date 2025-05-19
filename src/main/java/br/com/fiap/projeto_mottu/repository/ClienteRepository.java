5419d00b9da272e5c5989252a0eb75a6d740bfc9
dudatiemiak <rm554756@fiap.com.br>
2025-05-19 20:37:44 -0300

    Atualizando método e ordenando os resultados por nome do cliente

--- src/main/java/br/com/fiap/projeto_mottu/repository/ClienteRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/ClienteRepository.java
@@ -9,11 +9,13 @@ import br.com.fiap.projeto_mottu.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
+	//Busca cliente e ordena por nome
 	@Query(nativeQuery = true, value = """
-			select * from cliente c 
-			where c.nm_cliente like concat('%', :sub_cliente, '%')
-				or c.nr_cpf like concat('%', :sub_cliente, '%')
-				or c.nm_email like concat('%', :sub_cliente, '%')
-			""")
-	public List<Cliente> buscarClientePorSubstring(String sub_cliente);
+	        select * from cliente c 
+	        where c.nm_cliente like concat('%', :sub_cliente, '%')
+	            or c.nr_cpf like concat('%', :sub_cliente, '%')
+	            or c.nm_email like concat('%', :sub_cliente, '%')
+	        order by c.nm_cliente asc
+	    """)
+	    public List<Cliente> buscarClientePorSubstringOrdenadoPorNome(String sub_cliente);
 }
