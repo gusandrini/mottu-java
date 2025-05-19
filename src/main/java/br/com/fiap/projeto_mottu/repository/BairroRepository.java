22bb87efc7be8f70c9c0be0144642a4398d62043
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 23:02:16 -0300

    Apagando importações desnecessárias

--- src/main/java/br/com/fiap/projeto_mottu/repository/BairroRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/BairroRepository.java
@@ -1,12 +1,8 @@
 package br.com.fiap.projeto_mottu.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import br.com.fiap.projeto_mottu.model.Bairro;
-import br.com.fiap.projeto_mottu.projection.BairroProjection;
-
-import java.util.List;
 
 public interface BairroRepository extends JpaRepository<Bairro, Long>{
 
