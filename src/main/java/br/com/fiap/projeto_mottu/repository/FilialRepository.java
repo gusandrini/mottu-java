8f806f4875ccdc48136734623290bb83bafe370d
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 21:51:23 -0300

    Atualizando FilialRepository, para buscar filiais pelo nome, não precisa ser exato

--- src/main/java/br/com/fiap/projeto_mottu/repository/FilialRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/FilialRepository.java
@@ -1,9 +1,15 @@
 package br.com.fiap.projeto_mottu.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.projeto_mottu.model.Filial;
 
 public interface FilialRepository extends JpaRepository<Filial, Long>{
 
+	// Busca por nome da filial
+    List<Filial> findByNomeFilialContainingIgnoreCase(String nomeFilial);
+
+    
 }
