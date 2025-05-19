021428d5504cd76f1cafccbe8927b5d4343bd203
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 23:09:21 -0300

    Apagando um dos métodos de busca, que buscava por nome um cliente

--- src/main/java/br/com/fiap/projeto_mottu/repository/ClienteRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/ClienteRepository.java
@@ -9,9 +9,6 @@ import br.com.fiap.projeto_mottu.model.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
-	@Query("from Cliente c where c.nm_cliente like concat('%', :nome, '%')")
-	List<Cliente> buscarClientePorNome(String nome);
-	
 	@Query(nativeQuery = true, value = """
 			select * from cliente c 
 			where c.nm_cliente like concat('%', :sub_cliente, '%')
