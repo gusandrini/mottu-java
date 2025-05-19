b41b2d6ee577bde9e1dc8dec40ac87e35774508f
dudatiemiak <rm554756@fiap.com.br>
2025-05-18 21:56:11 -0300

    Adicionando FuncionarioProjection

---
+++ src/main/java/br/com/fiap/projeto_mottu/projection/FuncionarioProjection.java
@@ -0,0 +1,8 @@
+package br.com.fiap.projeto_mottu.projection;
+
+public interface FuncionarioProjection {
+
+	String getNmFuncionario();
+    String getNmCargo();
+    String getNmEmailCorporativo();
+}
