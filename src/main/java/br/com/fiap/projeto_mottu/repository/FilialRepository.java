39888c507684c4bd441ec90b97ce0e12e1993ef5
dudatiemiak <rm554756@fiap.com.br>
2025-05-19 22:12:26 -0300

    Mudando os nomes novamente para ficar tudo de acordo

--- src/main/java/br/com/fiap/projeto_mottu/repository/FilialRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/FilialRepository.java
@@ -3,13 +3,13 @@ package br.com.fiap.projeto_mottu.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.fiap.projeto_mottu.model.Filial;
 
 public interface FilialRepository extends JpaRepository<Filial, Long>{
 
-	// Busca por nome da filial
-    List<Filial> findByNomeFilialContainingIgnoreCase(String nm_filial);
-
-    
+	@Query("SELECT f FROM Filial f WHERE LOWER(f.nm_filial) LIKE LOWER(CONCAT('%', :nomeFilial, '%'))")
+    List<Filial> buscarPorNome(@Param("nomeFilial") String nomeFilial);
 }
