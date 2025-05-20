062db4e4de63c42d46381fa303cf22ae1dc64193
dudatiemiak <rm554756@fiap.com.br>
2025-05-20 13:02:43 -0300

    Tirando a ordenação do método de busca e ignorando maisculas/minusculas e aceitando parte da palavra para o nome de busca

--- src/main/java/br/com/fiap/projeto_mottu/repository/DepartamentoRepository.java
+++ src/main/java/br/com/fiap/projeto_mottu/repository/DepartamentoRepository.java
@@ -4,13 +4,20 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.fiap.projeto_mottu.model.Departamento;
 import br.com.fiap.projeto_mottu.projection.DepartamentoProjection;
 
 public interface DepartamentoRepository extends JpaRepository<Departamento, Long>{
 
-	@Query("SELECT d.nm_departamento AS nmDepartamento, d.desc_departamento AS descDepartamento "
-	         + "FROM Departamento d WHERE d.nm_departamento = :nmDepartamento ORDER BY d.desc_departamento DESC")
-	    List<DepartamentoProjection> buscarDepartamentoPorNome(String nmDepartamento);
+	//Busca departamentos por nome
+	@Query("""
+		    SELECT 
+		        d.nm_departamento AS nmDepartamento,
+		        d.desc_departamento AS descDepartamento
+		    FROM Departamento d
+		    WHERE LOWER(d.nm_departamento) LIKE LOWER(CONCAT('%', :nmDepartamento, '%'))
+	""")
+	List<DepartamentoProjection> buscarDepartamentoPorNome(@Param("nmDepartamento") String nmDepartamento);
 }
