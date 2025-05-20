a34af028dec982a9b3efe9b853303c41c61b3e00
dudatiemiak <rm554756@fiap.com.br>
2025-05-20 09:25:39 -0300

    Removendo importação desnecessária

--- src/main/java/br/com/fiap/projeto_mottu/model/FilialDepartamento.java
+++ src/main/java/br/com/fiap/projeto_mottu/model/FilialDepartamento.java
@@ -10,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
